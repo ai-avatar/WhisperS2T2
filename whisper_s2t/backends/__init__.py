e8bf5c5cb24f3a26e12dcdb9e1c0d490b82ddd40
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-16 16:36:18 +0200

    Fixing align model for n_mels=128 models
    For Whisper tiny n_mels needs to be 80 instead of 128. So we need to use two different align models

--- whisper_s2t/backends/__init__.py
+++ whisper_s2t/backends/__init__.py
@@ -84,6 +84,7 @@ class WhisperModel(ABC):
 
         # Load Pre Processor
         self.preprocessor = LogMelSpectogram(n_mels=self.n_mels).to(self.device)
+        self.align_preprocessor = LogMelSpectogram(n_mels=80).to(self.device)
 
         # Load Speech Segmenter
         self.speech_segmenter = SpeechSegmenter(self.vad_model, device=self.device, **self.speech_segmenter_options)
@@ -168,7 +169,8 @@ class WhisperModel(ABC):
         with tqdm(total=len(audio_files)*100, desc=f"Transcribing") as pbar:
             for signals, prompts, seq_len, seg_metadata, pbar_update in self.data_loader(audio_files, lang_codes, tasks, initial_prompts, batch_size=batch_size):
                 mels, seq_len = self.preprocessor(signals, seq_len)
-                res = self.generate_segment_batched(mels.to(self.device), prompts, seq_len, seg_metadata)
+                align_mels, align_seq_len = self.align_preprocessor(signals, seq_len)
+                res = self.generate_segment_batched(mels.to(self.device), prompts, seq_len, seg_metadata, align_mels.to(self.device), align_seq_len)
 
                 for res_idx, _seg_metadata in enumerate(seg_metadata):
                     responses[_seg_metadata['file_id']].append({**res[res_idx],
