97ef3044897c42ba144f4b4920cf93333ed8a591
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-16 15:37:19 +0200

    Always load aligner model

--- whisper_s2t/backends/tensorrt/model.py
+++ whisper_s2t/backends/tensorrt/model.py
@@ -111,14 +111,13 @@ class WhisperModelTRT(WhisperModel):
         tokenizer_file = os.path.join(self.model_path, "tokenizer.json")
         tokenizer = Tokenizer(tokenizers.Tokenizer.from_file(tokenizer_file), self.model.is_multilingual)
 
-        if self.asr_options['word_timestamps']:
-            self.aligner_model_path = download_model(self.asr_options['word_aligner_model'])
-            self.aligner_model = ctranslate2.models.Whisper(self.aligner_model_path,
-                                                            device=device,
-                                                            device_index=device_index,
-                                                            compute_type=compute_type,
-                                                            intra_threads=cpu_threads,
-                                                            inter_threads=num_workers)
+        self.aligner_model_path = download_model(self.asr_options['word_aligner_model'])
+        self.aligner_model = ctranslate2.models.Whisper(self.aligner_model_path,
+                                                        device=device,
+                                                        device_index=device_index,
+                                                        compute_type=compute_type,
+                                                        intra_threads=cpu_threads,
+                                                        inter_threads=num_workers)
         
         self.generate_kwargs = {
             "end_id": tokenizer.eot,
