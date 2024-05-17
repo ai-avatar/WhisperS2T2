6e4e3bd5ddecb375dd8889a310a935ec91a60ea9
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-17 10:50:18 +0200

    TesnorRT: Fix alignment
    `align_seq_len` wasn't correctly set

--- whisper_s2t/backends/__init__.py
+++ whisper_s2t/backends/__init__.py
@@ -168,9 +168,9 @@ class WhisperModel(ABC):
         pbar_pos = 0
         with tqdm(total=len(audio_files)*100, desc=f"Transcribing") as pbar:
             for signals, prompts, seq_len, seg_metadata, pbar_update in self.data_loader(audio_files, lang_codes, tasks, initial_prompts, batch_size=batch_size):
-                mels, seq_len = self.preprocessor(signals, seq_len)
+                mels, main_seq_len = self.preprocessor(signals, seq_len)
                 align_mels, align_seq_len = self.align_preprocessor(signals, seq_len)
-                res = self.generate_segment_batched(mels.to(self.device), prompts, seq_len, seg_metadata, align_mels.to(self.device), align_seq_len)
+                res = self.generate_segment_batched(mels.to(self.device), prompts, main_seq_len, seg_metadata, align_mels.to(self.device), align_seq_len)
 
                 for res_idx, _seg_metadata in enumerate(seg_metadata):
                     responses[_seg_metadata['file_id']].append({**res[res_idx],
