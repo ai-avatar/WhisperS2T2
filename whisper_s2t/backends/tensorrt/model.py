1084fabb8ffe7b016959313b16501244cecf4ec2
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-24 21:08:08 +0200

    Return shorter segments

--- whisper_s2t/backends/tensorrt/model.py
+++ whisper_s2t/backends/tensorrt/model.py
@@ -278,9 +278,20 @@ class WhisperModelTRT(WhisperModel):
             print('text_tokens', text_tokens)
             sot_seqs = [tuple(_[-4:]) for _ in prompts]
             word_timings = self.align_words(align_features, texts, text_tokens, sot_seqs, align_seq_lens, seg_metadata)
-            print('word_timings', word_timings)
 
             for _response, _word_timings in zip(response, word_timings):
                 _response['word_timestamps'] = _word_timings
+
+            offset = 0
+            for idx, segment in enumerate(np.array(response).flat):
+                segment_length = len(segment['text'].replace(" ", ""))
+                words = []
+                for word_timing in word_timings[offset:]:
+                    words.append(word_timing)
+                    segment_length -= len(word_timing['word'])
+                    if segment_length <= 0:
+                        offset += len(words)
+                        break
+                segment['word_timestamps'] = words
 
         return response
