8ae72da2a59d9a80c2039d85c99e804b76b109f7
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-29 08:49:25 +0200

    Fix

--- whisper_s2t/backends/ctranslate2/model.py
+++ whisper_s2t/backends/ctranslate2/model.py
@@ -257,7 +257,7 @@ class WhisperModelCT2(WhisperModel):
         text_groups = self.tokenizer.decode_batch(tokens)
         print(group_timestamps)
         print(self.tokenizer.timestamp_begin)
-        print(self.tokenizer.decode_batch([group_timestamps]))
+        print(self.tokenizer.tokenizer.decode_batch([group_timestamps]))
 
         texts = []
         for idx, num_groups in enumerate(groups_per_segment):
