a9bfdd2599c356d34e2dfb48f94b3e5133e3cc45
Bruno Joseph <bruno.joseph95@gmail.com>
2024-05-29 13:49:38 +0200

    Debug IndexError

--- whisper_s2t/backends/__init__.py
+++ whisper_s2t/backends/__init__.py
@@ -145,8 +145,12 @@ class WhisperModel(ABC):
                 res = self.generate_segment_batched(mels.to(self.device), prompts, main_seq_len, seg_metadata, align_mels.to(self.device) if align_mels is not None else None, align_seq_len)
 
                 for segment in res:
-                    start_time = round(segment['word_timestamps'][0]['start'], 3) if word_timestamps else segment['start_time']
-                    end_time = round(segment['word_timestamps'][-1]['end'], 3) if word_timestamps else segment['end_time']
+                    try:
+                        start_time = round(segment['word_timestamps'][0]['start'], 3) if word_timestamps else segment['start_time']
+                        end_time = round(segment['word_timestamps'][-1]['end'], 3) if word_timestamps else segment['end_time']
+                    except:
+                        print("segment", segment)
+                        raise
                     responses[0].append({**segment,
                                          'start_time': start_time,
                                          'end_time': end_time})
