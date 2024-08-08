597f19f29f312892d346f39b2f31351cceeb61a5
Bruno Joseph <bruno.joseph95@gmail.com>
2024-08-08 09:21:25 +0200

    Remove print

--- whisper_s2t/backends/huggingface/model.py
+++ whisper_s2t/backends/huggingface/model.py
@@ -208,7 +208,6 @@ class WhisperModelHF(WhisperModel):
                                                 task=task,
                                                 language=lang,
                                                 **(self.generate_kwargs | generation_kwargs))
-            print("DONE GENERATING")
             # remove prompt tokens from the result
             if 'prompt_ids' in generation_kwargs and generation_kwargs['prompt_ids'] is not None:
                 result = [segment[len(generation_kwargs['prompt_ids']):] for segment in result]
