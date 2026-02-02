301951d6ac4b629840bc8e9500e7b48412d5dbd9
Bruno Joseph <bruno.joseph95@gmail.com>
2026-02-02 21:12:03 +0100

    Fix

--- whisper_s2t/backends/ctranslate2/model.py
+++ whisper_s2t/backends/ctranslate2/model.py
@@ -261,7 +261,7 @@ class WhisperModelCT2(WhisperModel):
             for logit_array in segment.logits:
                 for logit in logit_array:
                     if isinstance(logit, ctranslate2.StorageView):
-                        logits.append(torch.as_tensor(logit.to_device(ctranslate2.Device(0))))
+                        logits.append(torch.as_tensor(logit.to_device(ctranslate2.Device(0)), dtype=torch.float32))
                         continue
                     logits.append(torch.tensor(np.array(logit.to_device(ctranslate2.Device(0)))))
             
