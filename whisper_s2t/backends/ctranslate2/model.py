865b4cb00231013ccaff6b7bcab54a57a86074e6
Bruno Joseph <bruno.joseph95@gmail.com>
2026-02-03 13:11:37 +0100

    Update comment

--- whisper_s2t/backends/ctranslate2/model.py
+++ whisper_s2t/backends/ctranslate2/model.py
@@ -262,7 +262,8 @@ class WhisperModelCT2(WhisperModel):
                 for logit in logit_array:
                     print(logit)
                     if isinstance(logit, ctranslate2.StorageView):
-                        # [cpu:0 float32 storage viewed as ]
+                        # This is the case when beam_size > 1. In this case, the logits seem to be empty, so we skip it.
+                        # logit: [cpu:0 float32 storage viewed as ]
                         continue
                     logits.append(torch.tensor(np.array(logit.to_device(ctranslate2.Device(0)))))
             
